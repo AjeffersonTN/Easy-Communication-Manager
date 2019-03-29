6d47682782e621bbe45e81b35d2413258ef264d0
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-03-28 19:12:31 -0500

    updated user login

--- src/components/DisplayComponents/LoginPage.js
+++ src/components/DisplayComponents/LoginPage.js
@@ -4,9 +4,9 @@ import DriverManager from "../../modules/DriverManager"
 class LoginPage extends React.Component {
     //set initial state
     state = {
-      fullName:"",
       email:"",
-      password: ""
+      password: "",
+      type_id:""
     };
 
     //update state whenever an input field is edited
@@ -16,23 +16,29 @@ class LoginPage extends React.Component {
         this.setState(stateToChange)
 
     };
+    handleTypeChange = evt => {
+        const stateToChange = {};
+        stateToChange[evt.target.id] = evt.target.id;
+        this.setState(stateToChange)
+
+    };
 
     handleLogin = e => {
         e.preventDefault()
-        if (this.state.email && this.state.password) {
-          DriverManager.searchUP(this.state.email, this.state.password).then(
+        if (this.state.email && this.state.password && this.state.type_id) {
+          DriverManager.searchUP(this.state.email, this.state.password, this.state.type_id).then(
             user => {
               if (!user.length) {
                 alert("Wrong email or password!")
               } else {
                 sessionStorage.setItem("credentials", parseInt(user[0].id))
                 this.props.history.push("/AssignEditLoads")
-                // .then(() => this.props.history.push("/AssignEditLoads"))
+
               }
             }
           )
         } else {
-          alert("Please Fill Out Form 😬!")
+          alert("Please Enter Login Information!")
         }
       }
 
@@ -65,6 +71,18 @@ class LoginPage extends React.Component {
                     />
                     <br></br>
 
+                    <label>Account Type:</label>
+                    <select
+                    defaultValue=""
+                    name="type_id"
+                    id="type_id"
+                    onChange={this.handleTypeChange}
+                    >
+                    <option value="">Select Account Type</option>
+                    <option id="1">Driver</option>
+                    <option >Dispatcher</option>
+            </select>
+            <br></br>
                     <button
                     type="submit"
                     onClick= {this.handleLogin}
